d1c4cec75cf7d797e592d2d9ec00462367053a1c
alaaynassar7 <alaaynassar7@gmail.com>
2026-02-07 20:48:46 +0200

    fix: update eks node group ami to Amazon Linux 2023

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,17 +1,3 @@
-# 1. EKS Cluster
-resource "aws_eks_cluster" "main" {
-  name     = "${var.project_name}-cluster"
-  role_arn = var.cluster_role_arn
-  version  = "1.29"
-
-  vpc_config {
-    subnet_ids              = var.subnet_ids
-    security_group_ids      = [var.node_sg_id]
-    endpoint_private_access = true
-    endpoint_public_access  = true
-  }
-}
-
 # 2. Managed Node Group 
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
@@ -24,6 +10,8 @@ resource "aws_eks_node_group" "main" {
     max_size     = 3
     min_size     = 1
   }
+
+  ami_type       = "AL2023_x86_64_STANDARD"
 
   instance_types = ["t3.small"]
   capacity_type  = "ON_DEMAND"
