83a8fbbc998774db4890af74285b45a7329e9b96
AzurePipeline <pipeline-bot@alaa.com>
2026-02-10 01:24:00 +0200

    Final Infrastructure Fixes: Cognito, Network Tags, and Variable Cleanup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cd"
+    bucket = "alaa-devops-project-terraform-state"
     key    = "terraform.tfstate"                   
     region = "us-east-1"
     encrypt = true
