88f7421e3a22fd5d4530631e3df8aa869635c0d4
AzurePipeline <pipeline-bot@alaa.com>
2026-02-11 01:00:33 +0200

    fix(terraform): Add conditional logic for API Gateway integration

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -22,22 +22,30 @@ resource "aws_apigatewayv2_vpc_link" "main" {
 }
 
 resource "aws_apigatewayv2_integration" "main" {
+  count = length(regexall("^arn:aws:elasticloadbalancing", var.integration_uri)) > 0 ? 1 : 0
+
   api_id           = aws_apigatewayv2_api.main.id
   integration_type = "HTTP_PROXY"
-  integration_uri  = var.integration_uri 
   
   integration_method = "ANY"
   
+  # We don't need complex conditions here because we only enter this block if we have an ARN
   connection_type    = "VPC_LINK"
+  integration_uri    = var.integration_uri
   connection_id      = aws_apigatewayv2_vpc_link.main.id
   
   payload_format_version = "1.0"
 }
 
 resource "aws_apigatewayv2_route" "main" {
+  # Same logic: Do not create the route if there is no Integration
+  count = length(regexall("^arn:aws:elasticloadbalancing", var.integration_uri)) > 0 ? 1 : 0
+
   api_id             = aws_apigatewayv2_api.main.id
   route_key          = "ANY /{proxy+}"
-  target             = "integrations/${aws_apigatewayv2_integration.main.id}"
+  
+  # ⚠️ Important: We must use [0] because count turns this into a List
+  target             = "integrations/${aws_apigatewayv2_integration.main[0].id}"
   
   authorization_type = "JWT"
   authorizer_id      = aws_apigatewayv2_authorizer.main.id
