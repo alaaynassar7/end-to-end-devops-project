96db0872c1c1caa90e11d9befdbdfeb2e77ec083
Alaa Nassar <145594145+alaaynassar7@users.noreply.github.com>
2026-02-11 14:33:38 +0200

    "fix: comment access entry and update principal_arn"

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -108,16 +108,15 @@ resource "aws_eks_node_group" "main" {
   )
 }
 
-resource "aws_eks_access_entry" "root" {
-  cluster_name  = aws_eks_cluster.main.name
-  principal_arn = var.principal_arn
-  type          = "STANDARD"
-}
-
+# resource "aws_eks_access_entry" "root" {
+#   cluster_name  = aws_eks_cluster.main.name
+#   principal_arn = var.principal_arn
+#   type          = "STANDARD"
+# }
 resource "aws_eks_access_policy_association" "root" {
   cluster_name  = aws_eks_cluster.main.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = aws_eks_access_entry.root.principal_arn
+    principal_arn = var.principal_arn 
 
   access_scope {
     type = "cluster"
