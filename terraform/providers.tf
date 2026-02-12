87461e6ae8a53b88d47bdaf9dad50e68d8fc48e4
AzurePipeline <pipeline-bot@alaa.com>
2026-02-12 02:54:45 +0200

    Final Fix: Corrected providers mapping and set region to us-east-2

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,17 +14,18 @@ terraform {
       version = "> 2.11"
     }
   }
- }
+}
+
 provider "aws" {
-  region = "us-east-2"   
+  region = "us-east-2" 
 }
 
 data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.main.name
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.main.token
 }
